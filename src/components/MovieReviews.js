5a01380539011ec2a2faccac0a06e8742131bef8
Katrina Pritchard <pritckat@gmail.com>
2018-11-16 22:51:57 +0000

    automatically backed up by learn

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,11 @@
 // Code MovieReviews Here
+import React from 'react';
+
+const MovieReview = (
+  { headline, byline, link, summary_short }
+) => {
+  return(
+    <div key={headline} className="review">
+      <header>
+        <a className="review-link"
+  )
