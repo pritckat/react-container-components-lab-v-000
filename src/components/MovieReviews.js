ea056bfbefef75bf7796fdda9d5459897ef697db
Katrina Pritchard <pritckat@gmail.com>
2018-11-16 22:55:37 +0000

    automatically backed up by learn

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -11,3 +11,11 @@ const MovieReview = (
       </header>
   )
 }
+
+const MovieReviews = ({ reviews }) => <div className="review-list">{reviews.map(MovieReview)}</div>
+
+MovieReviews.defaultProps = {
+  reviews: []
+}
+
+export default MovieReviews
